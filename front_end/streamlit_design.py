14204be0d8b596e8c518e6a7f831cba8fa59ef09
Alina Linscheidt <a@linscheidt.org>
2024-05-30 17:51:26 +0200

    Design changes

--- front_end/streamlit_design.py
+++ front_end/streamlit_design.py
@@ -2,6 +2,12 @@ import streamlit as st
 import requests
 from PIL import Image
 
+im = Image.open("garden-guru-favicon.png")
+st.set_page_config(
+    page_title="Garden Guru",
+    page_icon=im,
+)
+
 # Using CSS to change background color
 st.markdown(
     """
@@ -36,40 +42,45 @@ prediction_url = "http://127.0.0.1:8000/prediction"
 chat_url = "http://127.0.0.1:8000/chat"
 
 # Home endpoint response
-st.header("Welcome, let yourself be guided on your green journey: ✨")
-home_response = requests.get("http://127.0.0.1:8000/")
-st.write(home_response.json().get("message", "No welcome message available."))
+st.header("Welcome to")
 
 # Setting up the Streamlit Application Title
 st.title("Garden Guru 🪴")
 
+# Home endpoint response
+st.header("Let me guide you on your green journey. ✨")
+home_response = requests.get("http://127.0.0.1:8000/")
+st.write(home_response.json().get("message", "No welcome message available."))
+
 # Image upload section
-st.header("Show your plant") # Adds a header for the image upload section
+st.header("Show me your plant:") # Adds a header for the image upload section
 uploaded_file = st.file_uploader("Choose an image...", type="jpg") # Provides a file uploader widget that accepts JPG images
 
+plant_class = 'houseplant'
 # Handling the Uploaded Image
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
-    st.write("Classifying...")
+    st.write("Meditating on how to take care of your green protegé...")
 
     # Save the uploaded file to a temporary location
-    with open("raw_data/temp_image.jpg", "wb") as f:
-        f.write(uploaded_file.getbuffer())
+    # with open(f"raw_data/{uploaded_file.namefile}", "wb") as f:
+    #     f.write(uploaded_file.getbuffer())
     
     # Send image to FastAPI for prediction
-    with open("raw_data/temp_image.jpg", "rb") as f:
-        files = {"file": f}
-        response = requests.post(prediction_url, files=files)
-        care_tips = response.json().get("text", "No care tips available.")
+    # with open(f"raw_data/{uploaded_file.namefile}","rb") as f:
+    files = {"file": uploaded_file.getbuffer()}
+    response = requests.post(prediction_url, files=files).json()
+    care_tips = response.get("text", "No care tips available, follow your heart. 💚")
+    plant_class = response.get("plant")
 
     st.write(f"Care Tips: {care_tips}")
 
 # Chatbot section
-st.header("More answers needed? Fear not!")
+st.header("Need more answers? I am here to help. 😊")
 #plant_class = st.text_input("Enter the plant class (e.g., Begonia rex):")
-user_prompt = st.text_input("What would you like to know?")
+user_prompt = st.text_input("What would you like to find out?")
 
 # Handling the User's Question
 if st.button("Ask"):        # Adds a button that triggers the chat functionality
@@ -79,6 +90,6 @@ if st.button("Ask"):        # Adds a button that triggers the chat functionality
         chat_response = response.json().get("response", "No response available.")
         st.write(f"Expert Answer: {chat_response}")
     else:
-        st.write("Please enter both the plant class and your question.")
+        st.write("Please enter your question.")
 
 
